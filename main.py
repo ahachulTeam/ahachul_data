d0a508664bd5261c41f09df0ff3391524153ae6e
chanu <chanu48@naver.com>
2023-05-15 22:29:45 +0900

    update all.json

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ parser = argparse.ArgumentParser(description='choose crawl all or update new')
 parser.add_argument('-o', '--option', type=str, help='ca for crawl all, un for update new')
 args=parser.parse_args()
 
+print("Start Crawling")
 crawling.crawl(args.option)
