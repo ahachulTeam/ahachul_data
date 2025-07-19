bb5ce24ffa4edd45aa5820f147db4b5f67be3680
chanu48 <chanu48@naver.com>
2025-07-19 16:09:03 +0900

    feat: all.json S3에 저장 및 redis-stream 로깅 추가

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 ROOT = os.path.dirname(__file__)
 ROOTDATA = os.path.join(ROOT, 'datas')
@@ -6,3 +9,14 @@ LOSTURL = 'https://www.lost112.go.kr'
 STARTPAGE = 1
 START_YMD = '20250118'
 ALLDATA = 'all.json'
+
+# AWS S3 설정
+S3_ACCESS_KEY = os.getenv("access-key")
+S3_SECRET_KEY = os.getenv("secret-key")
+S3_BUCKET_NAME = os.getenv("bucket-name")
+S3_REGION = os.getenv("region")
+
+# Redis 설정
+REDIS_HOST = os.getenv("host")
+REDIS_PORT = int(os.getenv("port", 6379))
+REDIS_STREAM = os.getenv("stream")
